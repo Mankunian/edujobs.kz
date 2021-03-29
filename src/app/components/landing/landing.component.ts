3042f5043f1887e5a044ccfaf0091c0b3a2fcee8
web-dev <dias.07.04@gmail.com>
2021-03-29 21:20:40 +0600

    featured school section

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { ShareDataService } from "../../services/share-data.service";
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+
 @Component({
 	selector: 'app-landing',
 	templateUrl: './landing.component.html',
-	styleUrls: ['./landing.component.scss']
+	styleUrls: ['./landing.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class LandingComponent implements OnInit {
 	region: any;
 	job_title: any;
 	sponsoredJobs: any;
 	images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-	constructor(private router: Router, private shareData: ShareDataService) { }
+	featuredSchools: { id: number; title: string; img: string; }[];
+
+
+	constructor(private router: Router, private shareData: ShareDataService, config: NgbCarouselConfig) {
+		config.interval = 3000;
+		config.wrap = false;
+		config.keyboard = false;
+		config.pauseOnHover = false;
+		config.wrap = false;
+	}
 
 	ngOnInit(): void {
 		this.getSponsoredJobs();
+		this.getFeaturedJobs();
 	}
 
 	searchJobs() {
@@ -28,6 +42,15 @@ export class LandingComponent implements OnInit {
 			{ id: 1, title: 'Principal', location: 'Greater London', school: 'The Hyde school', price: 'MPS', time: 'Permanent' },
 			{ id: 2, title: 'Key stage 2 class Teacher', location: 'Manchester', school: 'The Hyde school', price: 'MPS', time: 'Permanent' },
 			{ id: 3, title: 'Lecturer in Digital Computer', location: 'Greater London', school: 'The Hyde school', price: 'MPS', time: 'Permanent' }
+		]
+	}
+
+	getFeaturedJobs() {
+		this.featuredSchools = [
+			{ id: 1, title: 'The Pivot Academy', img: 'assets/img/custom/1.jpg' },
+			{ id: 2, title: 'The Hyde School', img: 'assets/img/custom/2.png' },
+			{ id: 3, title: 'Xavier Catholic Education Trust', img: 'assets/img/custom/3.jpg' },
+			{ id: 4, title: 'Eton Dorney Independent School', img: 'assets/img/custom/4.jpg' }
 		]
 	}
 }
