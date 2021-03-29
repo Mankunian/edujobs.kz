c2a3786a3a402aa171e78753dc33f8faf8dfd6ef
web-dev <dias.07.04@gmail.com>
2021-03-29 15:15:12 +0600

    ii

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -25,7 +25,9 @@ export class LandingComponent implements OnInit {
 
 	getSponsoredJobs() {
 		this.sponsoredJobs = [
-			{ id: 1, title: 'Principal', location: 'Greater London', }
+			{ id: 1, title: 'Principal', location: 'Greater London', school: 'The Hyde school', price: 'MPS', time: 'Permanent' },
+			{ id: 2, title: 'Key stage 2 class Teacher', location: 'Manchester', school: 'The Hyde school', price: 'MPS', time: 'Permanent' },
+			{ id: 3, title: 'Lecturer in Digital Computer', location: 'Greater London', school: 'The Hyde school', price: 'MPS', time: 'Permanent' }
 		]
 	}
 }
