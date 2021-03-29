700944b0850cbd6c34e2c2022663b5ef4060788a
web-dev <dias.07.04@gmail.com>
2021-03-29 14:08:03 +0600

    navbar toggle

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -10,14 +10,22 @@ import { ShareDataService } from "../../services/share-data.service";
 export class LandingComponent implements OnInit {
 	region: any;
 	job_title: any;
+	sponsoredJobs: any;
 	constructor(private router: Router, private shareData: ShareDataService) { }
 
 	ngOnInit(): void {
+		this.getSponsoredJobs();
 	}
 
 	searchJobs() {
 		let search_params = { 'region': this.region, 'job_title': this.job_title };
 		this.shareData.changeMessage(JSON.stringify(search_params))
 		this.router.navigate(['/main'])
+	}
+
+	getSponsoredJobs() {
+		this.sponsoredJobs = [
+			{ id: 1, title: 'Principal', location: 'Greater London', }
+		]
 	}
 }
