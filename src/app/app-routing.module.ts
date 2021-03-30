d58c68cef6dad57b666eefce1eae617bc55c07ce
web-dev <dias.07.04@gmail.com>
2021-03-30 23:09:15 +0600

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
 import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
 	{ path: '', component: LandingComponent },
 	{ path: 'landing', component: LandingComponent },
 	{ path: 'main', component: MainComponent },
+	{ path: 'login', component: LoginRegisterComponent },
 	{ path: '**', component: LandingComponent }
 ];
 // configures NgModule imports and exports
